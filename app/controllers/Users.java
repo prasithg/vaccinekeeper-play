40397077c8d9c0307c0afa2262cf4df84b1a9bd5
MichaelJaniak <michael.janiak@gmail.com>
2013-02-05 20:07:27 -0500

    Removed some comments from Users controller

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -1,28 +1,19 @@
 package controllers;
 
-import java.io.IOException;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 import models.Child;
 import models.Family;
-import models.Schedule;
 import models.User;
 
 import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
 
-import com.mongodb.MongoException;
-
-import play.data.validation.Constraints.EmailValidator;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Results;
-import play.mvc.BodyParser;                     
 
 
 public class Users extends Controller {
@@ -31,12 +22,6 @@ public class Users extends Controller {
 		return ok(views.html.index.render(getFamilies()));
 	}
 
-/*	public static Result getChild(String childId){
-		Child child = Child.findOneById(childId);
-		if (child == null ) return Results.notFound("The child id "+childId+" is not valid");
-		return ok(play.libs.Json.toJson(child));			
-	}
-*/
 	@BodyParser.Of(BodyParser.Json.class)
 	public static Result registerUser(){
 		JsonNode json = request().body().asJson();
